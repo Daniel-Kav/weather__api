8eff2a7c740ace3b08645fcea724fbfef5501203
daniel-kav <dankavatha01@gmail.com>
2023-09-16 17:02:17 +0300

    added a function to fetch weather data from openweather api

---
+++ main.js
@@ -0,0 +1,21 @@
+//function to fetch the weather data for a given location
+function getWeatherData(location){
+    const apiKey = '16e1b7f894186c09971dcb604e7c8d34';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+    fetch(apiUrl, {mode:"cors",})
+      .then((response){
+        if(!response.ok){
+            throw new Error('Network response failed');
+        }
+        return response.json();
+      })
+      .then((data) =>{
+        console.log('Weather data:',data);
+      })
+      .catch((err) => {
+        console.error('Error', err);
+      });
+}
+
+getWeatherData('Nairobi');
